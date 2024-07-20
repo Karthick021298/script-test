1fecd4d80b62d753a60482d14474e165b5b759c2
Karthick0298 <karthick.m@rigelsoft.com>
2024-07-20 11:46:43 +0530

    next-config aws

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,10 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
+        hostname: "java-files.s3.ap-south-1.amazonaws.com",
+      },
+      {
+        protocol: "https",
         hostname: "**",
       },
     ],
