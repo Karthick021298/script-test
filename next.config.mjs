15d1bf9205abfead2a8a60eb6a685daa426f4192
Karthick0298 <karthick.m@rigelsoft.com>
2024-07-20 11:28:02 +0530

    next-config

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-		remotePatterns: [
-			{
-				protocol: 'https',
-				hostname: 'ik.imagekit.io',
-			},
-            {
-				protocol: 'https',
-				hostname: 'java-files.s3.ap-south-1.amazonaws.com',
-			},           
-		],
-	},
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
